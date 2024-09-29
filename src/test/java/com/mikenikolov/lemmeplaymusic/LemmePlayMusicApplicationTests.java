38557469b96978732982c314b8122c05f7521594
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2024-09-30 01:00:08 +0300

    init: add project files

---
+++ src/test/java/com/mikenikolov/lemmeplaymusic/LemmePlayMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikenikolov.lemmeplaymusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemmePlayMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
